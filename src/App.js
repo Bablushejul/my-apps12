3f24d576565e44f578cbc1396412d4af24092ce2
Bablushejul <shejulbablu@gmail.com>
2023-03-22 16:20:46 +0530

    fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
- const [movies,setMovies]=useState([]);
-  function fetchMovieHandler(){
-    fetch('https://swapi.dev/api/films/').then((response)=>{
-return response.json();
+  const [movies, setMovies] = useState([]);
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
 
-    }).then((data)=>{
-      const transFormedMovies =data.results.map(moviedata=>{
-        return{
-        id:moviedata.episode_id,
-        title:moviedata.title,
-        openingText:moviedata.opening_crawl,
-        release_date:moviedata.release_date
-        }
+    const data = await response.json();
 
-      })
-setMovies(transFormedMovies)
+    const transFormedMovies = data.results.map((moviedata) => {
+      return {
+        id: moviedata.episode_id,
+        title: moviedata.title,
+        openingText: moviedata.opening_crawl,
+        release_date: moviedata.release_date,
+      };
     });
+    setMovies(transFormedMovies);
   }
 
   return (
